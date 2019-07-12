22a9829c781055d1cb955c2252acbbe6e22a0cea
prometheusXderpgang <prometheusXderpgang@gmail.com>
2019-07-12 10:38:52 +0530

    X00T: Fix RIL

--- syberia_X00T.mk
+++ syberia_X00T.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff
