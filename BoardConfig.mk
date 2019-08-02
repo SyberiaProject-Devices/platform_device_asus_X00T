d7c33b1f783c532c2d71c56d0ee37d1892e81310
prometheusXderpgang <prometheusXderpgang@gmail.com>
2019-08-02 16:39:48 +0530

    X00T: Comment SePolicy stuff
    We are building permisisve for now, so no need to include SePolicy stuff.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -242,8 +242,8 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2019-05-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
