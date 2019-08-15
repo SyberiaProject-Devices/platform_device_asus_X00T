SyberiaProject-Devices/platform_device_asus_X00T

commit e612b963cb2620286b1aeaeaf5a452c1edcdfc17

X00T: Fix commit 612d261d3c4fda066ce95a943d5e7c330088df94
Well, my bad

Signed-off-by: prometheusXderpgang <prometheusXderpgang@gmail.com>

vendor_prop.mk
@@ -48,7 +48,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.enable.splita2dp=true \
     ro.bluetooth.emb_wp_mode=true \
     ro.bluetooth.wipower=true \
-    vendor.qcom.bluetooth.soc=cherokee
+    vendor.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -89,7 +89,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.fm.a2dp.conc.disabled=false
-    vendor.bluetooth.soc=cherokee
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \

commit 918b16383e641bab32435503342d15123fe2dcef

X00T: Revert commit e612b963cb2620286b1aeaeaf5a452c1edcdfc17
Apparently commit e612b963cb2620286b1aeaeaf5a452c1edcdfc17 broke Bluetooth and FM altogether. And we obviously do not want that. So revert it.

This reverts commit e612b963cb2620286b1aeaeaf5a452c1edcdfc17.

Signed-off-by: prometheusXderpgang <prometheusXderpgang@gmail.com>

vendor_prop.mk
@@ -48,7 +48,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.enable.splita2dp=true \
     ro.bluetooth.emb_wp_mode=true \
     ro.bluetooth.wipower=true \
-    vendor.bluetooth.soc=cherokee
+    vendor.qcom.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -88,7 +88,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.fm.a2dp.conc.disabled=false
+    vendor.fm.a2dp.conc.disabled=false \
+    vendor.bluetooth.soc=cherokee
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \